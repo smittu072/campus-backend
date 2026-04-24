825a29c19a896c21a20f9f7131e34c33f75e4eb5
Patel Smit <145888005+smittu072@users.noreply.github.com>
2026-04-24 07:57:03 +0530

    fix multipart error

--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,10 @@
-from pymongo import MongoClient
 import os
+from pymongo import MongoClient
 
 MONGO_URL = os.getenv("MONGO_URL")
 
 if not MONGO_URL:
-    raise Exception("❌ MONGO_URL not found in environment variables")
+    raise Exception("MONGO_URL not found")
 
 client = MongoClient(MONGO_URL)
-
 db = client["campusDB"]
-
-print("✅ MongoDB Connected")
